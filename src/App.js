a5a2174e0c4e9cd0048eb405a509bd53ef928a8c
Raghunathji <partabrajp@gmail.com>
2021-04-23 00:41:51 +0500

    adding travis.yml to github

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					II RAAM II <code>Raghunathji</code> Jay Sia Raam
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Om Tryambakam Yajamahe, Sugandhim Pushti Vardhanam, Urvarukmiva
+					Bandhanan, Mrityur Mokshia Mamritat
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/II RAAM II/i);
   expect(linkElement).toBeInTheDocument();
 });
