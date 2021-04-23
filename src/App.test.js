ce97e442ff1d007f7e34cda512238d32fa5c6f5b
Raghunathji <partabrajp@gmail.com>
2021-04-23 22:55:15 +0500

    updating files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/II RAAM II/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/II RAAM II/i);
+	expect(linkElement).toBeInTheDocument();
 });
+
+// test('renders learn react link', () => {
+// 	const { getByText } = render(<App />);
+// 	const linkElement = getByText(/Hello World/i);
+// 	expect(linkElement.toBeInTheDocument());
+// });
